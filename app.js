3e6ff1b31f69b667e6df68e34f2ae9f2f76b9425
Muskaankamran <muskaankamran494@gmail.com>
2025-11-21 17:12:11 +0500

    e-commerce

--- app.js
+++ app.js
@@ -1,5 +1,62 @@
 var javascriptQuiz ={
-   ' question No 1':{
-            question: ""
-   }
+   'question No 1':{
+      text: "Which company developed JavaScript?",
+      choices:  ["Microsoft" ,   "Sun Microsystems" ,  " Netscape" , "IBM "] ,
+      correct: "Netscape",
+   },
+   'question No 2':{
+    text: "Which array method adds an item to the end?",
+    choices:  ["push()" ,   "pop()" ,  "shift() " , " unshift()"] ,
+    correct: "push()",
+ },
+ 'question No 3':{
+    text: "What is the output of: typeof null?",
+    choices:  ["object" ,   "null" ,  "undefined" , " string"] ,
+    correct: "object",
+ },
+ 'question No 4':{
+    text: "Which operator is used for strict equality?",
+    choices:  ["==" ,   "===" ,  "= " , " !="] ,
+    correct: "===",
+ },
+ 'question No 5':{
+    text: "JavaScript runs in the ______.",
+    choices:  ["Browser" ,   "TV" ,  "Keyboard" , "Speaker"] ,
+    correct: "",
+ },
+ 'question No 6':{
+    text: "Inside which HTML element do we put JavaScript?",
+    choices:  ["<javascript>" ,   "<script>" ,  "<js> " , " <code>"] ,
+    correct: "<script>",
+ },
+ 'question No 7':{
+    text: "What does DOM stand for?",
+    choices:  ["Document Object Model" ,   "Data Object Method" ,  "Desktop Oriented Mode " , "Document Oriented Markup"] ,
+    correct: "Document Object Model",
+ },
+ 'question No 8':{
+    text: "Which keyword declares a variable?",
+    choices:  ["var" ,   "let" ,  " const" , " All of the above"] ,
+    correct: "All of the above",
+ },
+ 'question No 9':{
+    text: "How do you write a comment in JavaScript?",
+    choices:  ["<!-- comment -->" ,   "# comment" ,  "// comment " , "$ comment "] ,
+    correct: "// comment ",
+ },
+ 'question No 10':{
+    text: "What is JavaScript used for?",
+    choices:  ["Styling webpages" ,   "Adding interactivity to webpages" ,  "Storing data on servers " , " Designing logos"] ,
+    correct: "Adding interactivity to webpages",
+ },
+ 'question No 11':{
+    text: "What file extension does JavaScript use?",
+    choices:  [".html" ,   ".css" ,  ".java " , " .js"] ,
+    correct: ".js",
+ },
+ 'question No 12':{
+    text: "Which method shows a pop-up message?",
+    choices:  ["alert()" ,   "show()" ,  "popup() " , "print() "] ,
+    correct: "",
+ },
 }
